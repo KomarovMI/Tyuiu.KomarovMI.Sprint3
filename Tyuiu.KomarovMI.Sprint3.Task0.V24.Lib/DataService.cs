ef0d60747d8458bd5d0f11c9c54a2b10de08dc29
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-10-18 00:04:33 +0500

    Add project files.

---
+++ Tyuiu.KomarovMI.Sprint3.Task0.V24.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KomarovMI.Sprint3.Task0.V24.Lib
+{
+    public class DataService : ISprint3Task0V24
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double multSeries = 1;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                multSeries = multSeries * (Math.Pow(3 / (startValue + Math.Pow(value, -startValue)),2));
+            }
+            return Math.Round(multSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KomarovMI.Sprint3.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMI.Sprint3.Task0.V24.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint3.Task0.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 7;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 372529.03;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
