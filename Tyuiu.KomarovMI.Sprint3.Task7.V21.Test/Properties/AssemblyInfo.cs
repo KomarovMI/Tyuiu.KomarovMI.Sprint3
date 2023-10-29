bbf5762248f9879e26313b9e54c1ebfc4d5b91d5
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-10-29 17:17:33 +0500

    Task7

---
+++ Tyuiu.KomarovMI.Sprint3.Task7.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomarovMI.Sprint3.Task7.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomarovMI.Sprint3.Task7.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad730697-7e61-484d-8d1f-2dc914424f3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
